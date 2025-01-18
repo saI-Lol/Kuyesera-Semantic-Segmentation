07279a1c64a29146af1ed287c951051ae894917d
ochwo laban <labanochwo@gmail.com>
2025-01-18 15:57:29 +0300

    added changes

--- train.py
+++ train.py
@@ -2,18 +2,134 @@ from model import SeResNext50_Unet_Loc
 import argparse
 import rasterio
 import torch
+from adamw import AdamW
+from torch.amp import autocast, GradScaler
+from tqdm import tqdm
+from utils import mae_loss, AverageMeter, TrainDataset, ValDataset
+import os
+from torch.utils.data import DataLoader
+
+def train_epoch(model, train_data_loader, optimizer, epoch):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    losses = AverageMeter()
+    scaler = GradScaler()
+
+    iterator = tqdm(train_data_loader)
+    model.train()
+
+    for i, batch in enumerate(iterator):
+        imgs = batch["img"].cuda(non_blocking=True)
+        msks = batch["msk"].cuda(non_blocking=True)
+        print(imgs.shape, msks.shape)
+
+        with autocast(device_type=device):
+            out = model(imgs)
+            loss_dict = mae_loss(out, msks)
+            loss = loss_dict['all']
+
+        losses.update(loss.item(), imgs.size(0))
+        iterator.set_description(f"Epoch: {epoch+1}, Loss: {losses.avg:.4f}")
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+def val_epoch(model, val_data_loader, epoch):
+    model = model.eval()
+    losses = AverageMeter()
+
+    with torch.no_grad():
+        for i, batch in enumerate(tqdm(val_data_loader)):
+            msks = batch["msk"].cuda(non_blocking=True)
+            imgs = batch["img"].cuda(non_blocking=True)
+            
+            out = model(imgs)
+            loss_dict = mae_loss(out, msks)
+            loss = loss_dict['all']
+            losses.update(loss.item(), imgs.size(0))
+
+    print(f"Epoch: {epoch+1}, Val Mae: {losses.avg:.4f}")
+    if best_mae is None:
+        best_mae = losses.avg
+    else:
+        if losses.avg < best_mae:
+            best_mae = losses.avg
+            torch.save({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'best_mae': best_mae,
+            }, os.path.join(models_folder, snapshot_name + '_best'))
+
+
+def evaluate(model, test_data_loader, damage_types):
+    model = model.eval()
+    losses = AverageMeter()
+    damage_losses = {damage_type: AverageMeter() for damage_type in damage_types}
+
+    with torch.no_grad():
+        for i, batch in enumerate(tqdm(test_data_loader)):
+            msks = batch["msk"].cuda(non_blocking=True)
+            imgs = batch["img"].cuda(non_blocking=True)
+            
+            out = model(imgs)
+            loss_dict = mae_loss(out, msks)
+            loss = loss_dict['all']
+            losses.update(loss.item(), imgs.size(0))
+            for damage_type in damage_types:
+                damage_losses[damage_type].update(loss_dict[damage_type].item(), imgs.size(0))
+
+    print(f"Test Mae all: {losses.avg:.4f}")
+    for damage_type in damage_types:
+        print(f"Test Mae {damage_type}: {damage_losses[damage_type].avg:.4f}")
+
+
+def main(args):
+    seed = args.seed 
+    batch_size = args.batch_size
+    val_batch_size = args.val_batch_size
+    epochs = args.epochs
+    workers = args.num_workers
+    damage_types = args.damage_types
+
+    snapshot_name = f'SeResNext50_Unet_Loc_{seed}'
+    models_folder = "/kaggle/working/models"
+
+    np.random.seed(seed)
+    random.seed(seed)
+
+    data_train = TrainDataset(args.train_data_paths, damage_types)
+    val_train = ValDataset(args.val_data_paths, damage_types)
+    data_test = ValDataset(args.test_data_paths, damage_types)
+
+    train_data_loader = DataLoader(data_train, batch_size=batch_size, num_workers=workers,  pin_memory=False, drop_last=True)
+    val_data_loader = DataLoader(val_train, batch_size=val_batch_size, num_workers=workers, pin_memory=False)
+    test_data_loader = DataLoader(data_test, batch_size=val_batch_size, num_workers=workers, pin_memory=False)
+
+    model = SeResNext50_Unet_Loc().cuda()
+    params = model.parameters()
+    optimizer = AdamW(params, lr=0.00015, weight_decay=1e-6)
+
+    best_mae = None
+
+    torch.cuda.empty_cache()
+    for epoch in range(epochs):
+        train_epoch(model, train_data_loader, optimizer, epoch)
+        val_epoch(model, val_data_loader, epoch)
+        torch.cuda.empty_cache()
+    evaluate(model, test_data_loader, damage_types)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train Segemntation Model")
-    parser.add_argument("--image-path", type=str, required=True, help="Path to the image file")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--batch-size", type=int, default=8)
+    parser.add_argument("--val-batch-size", type=int, default=8)
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument('--damage-types', nargs='+', required=True)
+    parser.add_argument('--train-data-paths', nargs='+', required=True)
+    parser.add_argument('--val-data-paths', nargs='+', required=True)
+    parser.add_argument('--test-data-paths', nargs='+', required=True)
     args = parser.parse_args()
-
-    model = SeResNext50_Unet_Loc().cuda()
-    with rasterio.open(args.image_path) as src:
-        img_array = src.read()
-    image_tensor = torch.from_numpy(img_array).float().cuda()
-    image_tensor = image_tensor.unsqueeze(0)
-    output = model(image_tensor)
-    print(output.size())
-    print(output)
+    main(args)
---
+++ utils.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+import os
+import rasterio
+import torch
+from torch.utils.data import Dataset
+import cv2
+import numpy as np
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def mae_loss(input, target):
+    pass
+
+def mask_for_polygon(poly, im_size=(1024, 1024)):
+    img_mask = np.zeros(im_size, np.uint8)
+    int_coords = lambda x: np.array(x).round().astype(np.int32)
+    exteriors = [int_coords(poly.exterior.coords)]
+    interiors = [int_coords(pi.coords) for pi in poly.interiors]
+    cv2.fillPoly(img_mask, exteriors, 1)
+    cv2.fillPoly(img_mask, interiors, 0)
+    return img_mask
+
+def get_damage_mask(damage_types, image_file_path):
+    damage_dict = {damage_type:idx for idx, damage_type in enumerate(damage_types, start=1)}
+    msk_damage = np.zeros((1024, 1024), dtype='uint8')
+    label_file_path = str(image_file_path).replace("images", "labels").replace(".tif", ".json")
+    with open(label_file_path, "r") as f:
+        json_data = json.load(f)
+    for item in json_data['features']['xy']:
+        subtype = item['properties']['subtype'].replace('-', '_')
+        poly = wkt.loads(item['wkt'])
+        _msk = mask_for_polygon(poly)
+        msk_damage[_msk > 0] = damage_dict.get(subtype, 0)
+    return msk_damage
+
+
+class TrainDataset(Dataset):
+    def __init__(self, train_data_paths, damage_types):
+        super().__init__()
+        image_ids = []
+        for train_data_path in train_data_paths:
+            train_data_path = Path(train_data_path)
+            for filename in os.listdir(train_data_path/ "images"):
+                image_ids.append(train_data_path/ "images" / f"{'_'.join(filename.split('_')[:-2])}_post_disaster.tif")
+        image_ids = sorted(set(image_ids))
+        self.image_ids = list(image_ids)
+        self.damage_types = damage_types
+
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        with rasterio.open(image_id) as f:
+            img = f.read()
+        img_tensor = torch.from_numpy(img).float()
+        msk = get_damage_mask(self.damage_types, image_id)
+        item = {'img': img_tensor, 'msk': msk, 'fn': str(image_id)}
+        return item
+
+
+    
+class ValDataset(Dataset):
+    def __init__(self, val_data_paths, damage_types):
+        super().__init__()
+        image_ids = []
+        for val_data_path in val_data_paths:
+            val_data_path = Path(val_data_path)
+            for filename in os.listdir(val_data_path/ "images"):
+                image_ids.append(val_data_path/ "images" / f"{'_'.join(filename.split('_')[:-2])}_post_disaster.tif")
+        image_ids = sorted(set(image_ids))
+        self.image_ids = list(image_ids)
+        self.damage_types = damage_types
+
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        with rasterio.open(image_id) as f:
+            img = f.read()
+        img_tensor = torch.from_numpy(img).float()
+        msk = get_damage_mask(self.damage_types, image_id)
+        item = {'img': img_tensor, 'msk': msk, 'fn': str(image_id)}
+        return item
