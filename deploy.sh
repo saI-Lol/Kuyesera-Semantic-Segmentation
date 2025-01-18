1771e677f66fd2807ec8381878e8ac37f2191637
ochwo laban <labanochwo@gmail.com>
2025-01-18 10:35:13 +0300

    added changes

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git checkout main
+git add .
+git commit -m "added changes"
+git push origin main
---
+++ train.py
@@ -0,0 +1,14 @@
+from .model import SeResNext50_Unet_Loc
+import argparse
+from torchvision.io import read_image
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train Segemntation Model")
+    parser.add_argument("--image-path", type=str, required=True, help="Path to the image file")
+    args = parser.parse_args()
+
+    model = SeResNext50_Unet_Loc().cuda()
+    image_tensor = read_image(args.image_path).unsqueeze(0).cuda()
+    output = model(image_tensor)
+    print(output)
